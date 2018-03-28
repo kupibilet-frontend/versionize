ef4a8ec42bc29226f184c1e5a10de0eb44eeeeaa
VladBlow <vladimir.tsibizow@gmail.com>
2018-03-28 13:30:03 +0300

    Added validate version by semver

--- index.js
+++ index.js
@@ -3,18 +3,20 @@
 const shell = require('shelljs')
 const slug = require('slug')
 const finder = require('find-package-json')
+const semver = require('semver')
 
 slug.charmap['/'] = '-'
 
 const finding = finder()
 const { version } = finding.next().value
+const validVersion = semver.valid(semver.coerce(version))
 
 const currentBranch = shell.exec('git rev-parse --abbrev-ref HEAD', { silent: true }).stdout
 const lastCommitHash = shell.exec("git log --pretty=format:'%h' -n 1", { silent: true }).stdout
 
 const slugRefBranch = slug(currentBranch).trim()
 
-const buildVersion = `${version}-${slugRefBranch}-${lastCommitHash}`
+const buildVersion = `${validVersion}-${slugRefBranch}-${lastCommitHash}`
 
 shell.echo(`new version of package: ${buildVersion}`)
 shell.exec(`npm version ${buildVersion}`)
